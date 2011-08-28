a2d6581f78e62e1a43279e8bea89d4e092b893d2
Isao Sonobe <sonoisa@gmail.com>
2011-08-28 16:57:23 +0900

    fixed typos

--- unpacked/extensions/TeX/xypic.js
+++ unpacked/extensions/TeX/xypic.js
@@ -90,10 +90,10 @@ MathJax.Extension.xypic = {
   }
 }
 
-// "TeX Xy-pic" depends "Functional Programming library" and "TeX Jax".
+// "TeX Xy-pic" depends on "Functional Programming library" and "TeX Jax".
 MathJax.Extension.xypic.signalHandler.chainSignal("TeX Xy-pic Require", ["Functional Programming library Ready", "TeX Jax Ready"]);
 
-// "HTML-CSS Xy-pic" depends "TeX Xy-pic" and "HTML-CSS Jax".
+// "HTML-CSS Xy-pic" depends on "TeX Xy-pic" and "HTML-CSS Jax".
 MathJax.Extension.xypic.signalHandler.chainSignal("HTML-CSS Xy-pic Require", ["TeX Xy-pic Ready", "HTML-CSS Jax Ready"]);
 
 MathJax.Hub.Register.StartupHook("TeX Xy-pic Require",function () {
